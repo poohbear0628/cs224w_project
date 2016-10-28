9512ca72d22d3e5529aa988c404472b60a2cba9c
Vincent <v1invent@hotmail.com>
2016-10-28 13:41:35 -0700

    Missing code for some reason

--- read_pickle_data.py
+++ read_pickle_data.py
@@ -1,7 +1,12 @@
 import cPickle as pickle
 import os
 
+directory = "D:\\224w\\project\\liwc"
+dataset = []
 
 for filename in os.listdir(directory):
     path = os.path.join(directory, filename)
+    dataset.append(pickle.load(open(path, "rb")))
 
+for data in dataset:
+    print data
